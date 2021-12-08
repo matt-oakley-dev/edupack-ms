6d7e645489db891e6c2df8b65b6049868de6f6a3
matt-oakley-dev <matt.oakley@wearesmile.com>
2021-12-08 22:27:33 +0000

    This is a new commit for what I originally planned to be amended

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::get('authentication_test', function() {
+        return 'Authenticated';
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
